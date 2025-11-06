27493477dfea3f834a12db6b84a226f557c5a5a9
mateushlsilva <mateushls01@gmail.com>
2025-11-06 10:05:23 -0300

    feat: configuração inicial do servidor Express com PostgreSQL e ambiente Docker Compose

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:alpine
+
+EXPOSE 3001
+
+WORKDIR /opt/server
+
+COPY ./src /opt/server/src
+COPY *.json /opt/server/
+
+ENV DB_HOST=localhost
+ENV DB_PORT=5432
+ENV PORT=3001
+ENV DB_USER=postgres
+ENV DB_PASS=postgres
+ENV DB_NAME=gntech_db
+ENV REDIS_HOST=localhost
+ENV REDIS_PORT=6379
+
+RUN npm i
+
+CMD [ "npm", "start" ]
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import * as dotenv from "dotenv";
+//import routes from "./routes";
+
+
+dotenv.config();
+
+
+const app = express();
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+//app.use(routes);
+
+const PORT = process.env.PORT || 3001;
+
+// inicializa o servidor na porta especificada
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
